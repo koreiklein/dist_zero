4e398079af3dbd85e2045b6f1516ea16bdd6b379
Korei Klein <korei.klein@gmail.com>
2018-04-10 19:08:00 -0400

    Update format.

--- dist_zero/errors.py
+++ dist_zero/errors.py
@@ -1,16 +1,19 @@
 import traceback
 
+
 class DistZeroError(Exception):
   '''
   Base class for all errors known to DistZero.
   '''
   pass
 
+
 class InternalError(DistZeroError):
   '''
   For errors internal to dist zero.
   '''
   pass
+
 
 class SimulationError(DistZeroError):
   def __init__(self, log_lines, exc_info):
@@ -23,8 +26,6 @@ class SimulationError(DistZeroError):
     tb_lines = traceback.format_tb(e_tb)
     exn_lines = traceback.format_exception_only(e_type, e_value)
     msg = "{}\nAfter simulated trace:\n\t{}\nA node raised an error:  {}".format(
-        ''.join(tb_lines),
-        '\n\t'.join(log_lines),
-        ''.join(exn_lines))
+        ''.join(tb_lines), '\n\t'.join(log_lines), ''.join(exn_lines))
 
     super(SimulationError, self).__init__(msg)
