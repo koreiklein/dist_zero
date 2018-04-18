7c19dccda51957f8a8c09b51daac2ecbee2fde67
Korei Klein <korei.klein@gmail.com>
2018-04-18 09:39:57 -0400

    Improve logging in the aws cloud spawner.

--- dist_zero/spawners/cloud/aws.py
+++ dist_zero/spawners/cloud/aws.py
@@ -91,8 +91,8 @@ class Ec2Spawner(spawner.Spawner):
   def create_machines(self, machine_configs):
     # see http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.ServiceResource.create_instances
     # For available parameters.
-    logger.info("Creating an instance on on aws ec2")
     n_new_machines = len(machine_configs)
+    logger.info("Creating new instance(s) on on aws ec2. n={n_instances}", extra={'n_instances': n_new_machines})
     instances = self._ec2_resource.create_instances(
         #BlockDeviceMappings=self._instance_block_device_mappings(),
         #Placement=self._instance_placement(),
@@ -121,9 +121,12 @@ class Ec2Spawner(spawner.Spawner):
     extra = {
         'machine_name': machine_name,
         'machine_controller_id': machine_controller_id,
+        'aws_instance_id': instance.id,
+        'provisioning_aws_instance': True,
     }
     logger.info("Configuring instance '{machine_name}' id='{machine_controller_id}'", extra=extra)
 
+    logger.debug("Configuring machine specific tags", extra=extra)
     instance.create_tags(Tags=[
         {
             'Key': 'Name',
@@ -138,12 +141,14 @@ class Ec2Spawner(spawner.Spawner):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.WarningPolicy())
     connected = False
+    logger.debug("Connecting to aws instance {aws_instance_id} over ssh", extra=extra)
     for i in range(5):
       try:
         ssh.connect(hostname=instance.public_dns_name, key_filename='.keys/dist_zero.pem', username='ec2-user')
         connected = True
         break
       except paramiko.ssh_exception.NoValidConnectionsError:
+        logger.debug("Ssh failed to connect to instance {aws_instance_id}", extra=extra)
         time.sleep(2)
 
     if not connected:
@@ -161,7 +166,7 @@ class Ec2Spawner(spawner.Spawner):
     ]:
       ssh.exec_command(command)
 
-    logger.info("Rsyncing dist_zero to aws instance", extra=extra)
+    logger.info("Rsyncing dist_zero files to aws instance {aws_instance_id}", extra=extra)
     # Do the rsync
     for precommand in [
         'rsync -avz -e "ssh -oStrictHostKeyChecking=no -i {keyfile}" dist_zero {user}@{instance}:/dist_zero/',
@@ -169,16 +174,25 @@ class Ec2Spawner(spawner.Spawner):
     ]:
 
       command = precommand.format(keyfile='.keys/dist_zero.pem', user='ec2-user', instance=instance.public_dns_name)
+      logger.debug(
+          "Running rsync command",
+          extra={
+              'aws_instance_id': instance.id,
+              'provisioning_aws_instance': True,
+              'command': command,
+          })
       os.system(command)
 
+    logger.debug("Running pip install", extra=extra)
     ssh.exec_command('cd /dist_zero; pip3 install --user -r requirements.txt')
 
+    logger.debug("Copying relevant environment variables", extra=extra)
     ssh.exec_command('''cat << EOF > /dist_zero/.env\n\n{}\nEOF\n'''.format('\n'.join(
         "{}='{}'".format(variable, getattr(settings, variable)) for variable in settings.CLOUD_ENV_VARS)))
 
     command = "cd /dist_zero; nohup python3 -m dist_zero.machine_init '{machine_controller_id}' '{machine_name}' '{mode}' &".format(
         machine_controller_id=machine_controller_id, machine_name=machine_name, mode=spawners.MODE_CLOUD)
-    logger.info("Starting machine_init on aws instance", extra=extra)
+    logger.info("Starting a MachineController process on an aws instance", extra=extra)
     ssh.exec_command(command)
 
     handle = messages.os_machine_controller_handle(machine_controller_id)
@@ -187,6 +201,13 @@ class Ec2Spawner(spawner.Spawner):
     return handle
 
   def _instance_status_is_reachable(self, status):
+    '''
+    Check if a machine is reachable.
+
+    :param object status: An InstanceStatus object as returned by the boto3 API.
+    :return: True iff the status object indicates that the machine is reachable.
+    :rtype: bool
+    '''
     details = status['InstanceStatus']['Details']
     reachabilities = [detail for detail in details if detail['Name'] == 'reachability']
     if len(reachabilities) == 1:
@@ -196,45 +217,72 @@ class Ec2Spawner(spawner.Spawner):
 
     return False
 
-  def _wait_for_reachable_instances(self, instances):
-    logger.info(
-        "Waiting for aws instances to enter a 'reachable' status",
-        extra={'instance_ids': [instance.id for instance in instances]})
+  def _retry_loop(self, on_wait, on_fail, wait_time_sec=4, retries=10):
+    def _result(loop_iteration):
+      while True:
+        if loop_iteration():
+          return
+        retries -= 1
+        if retries >= 0:
+          on_wait()
+          time.sleep(wait_time_sec)
+        else:
+          on_fail()
 
-    while True:
-      resp = self._ec2.describe_instance_status(InstanceIds=[instance.id for instance in instances])
+    return _result
+
+  def _wait_for_reachable_instances(self, instances):
+    '''
+    Wait until the boto3 API indicates that the desired are reachable.
+
+    :param list instances: A list of aws instances.
+    '''
+    instance_ids = [instance.id for instance in instances]
+    logger.info("Waiting for aws instances to enter a 'reachable' status", extra={'instance_ids': instance_ids})
+
+    def on_wait():
+      logger.debug(
+          "aws instances were not all reachable.  Waiting and trying again.", extra={'instance_ids': instance_ids})
+
+    def on_fail():
+      logger.error("instances did not become reachable.", extra={'instance_ids': instance_ids})
+      raise RuntimeError("Instances did not become reachable")
+
+    @self._retry_loop(on_wait=on_wait, on_fail=on_fail)
+    def _loop():
+      resp = self._ec2.describe_instance_status(InstanceIds=instance_ids)
       if all(self._instance_status_is_reachable(status) for status in resp['InstanceStatuses']):
-        logger.info("The requested aws instances are now reachable")
-        break
-      time.sleep(4)
+        logger.info("The requested aws instances are now reachable", extra={'instance_ids': instance_ids})
+        return True
+      else:
+        return False
 
   def _wait_for_running_instances(self, instances):
-    logger.info(
-        "Waiting for aws instances to enter a 'running' state",
-        extra={'instance_ids': [instance.id for instance in instances]})
+    instance_ids = [instance.id for instance in instances]
+    logger.info("Waiting for aws instances to enter a 'running' state", extra={'instance_ids': instance_ids})
 
-    while True:
+    def on_wait():
+      logger.debug("instances were not all running.  Waiting and trying again.", extra={'instance_ids': instance_ids})
+
+    def on_fail():
+      logger.error("Instances did not become running.", extra={'instance_ids': instance_ids})
+      raise RuntimeError("Instances did not become running.")
+
+    @self.retry_loop(on_wait=on_wait, on_fail=on_fail)
+    def _loop():
       for instance in instances:
         instance.load()
+
       states = [instance.state['Name'] for instance in instances]
+
       if all(state == 'running' for state in states):
         logger.info("The requested aws instances are now running")
-        break
-
-      non_running = defaultdict(int)
-      for state in states:
-        if state != 'running':
-          non_running[state] += 1
-
-      logger.info(
-          "Not all instances were running", extra={
-              'non_running_instance_counts': non_running,
-          })
-      time.sleep(4)
+        return True
+      else:
+        return False
 
   def send_to_machine(self, machine, message, sock_type='udp'):
     instance = self._aws_instance_by_id[machine['id']]
-    instance.load()
 
     if sock_type == 'udp':
       dst = (instance.public_ip_address, settings.MACHINE_CONTROLLER_DEFAULT_UDP_PORT)
