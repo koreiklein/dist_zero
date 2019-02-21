69c1162d4991ee0d78d1197362c329e5c91f79a4
Korei Klein <korei.klein@gmail.com>
2019-02-21 16:58:41 -0500

    Refactor receive_start_subscription for clarity.

--- dist_zero/node/io/transactions/receive_start_subscription.py
+++ dist_zero/node/io/transactions/receive_start_subscription.py
@@ -20,42 +20,49 @@ class ReceiveStartSubscription(transaction.ParticipantRole):
         target_intervals={kid['id']: self._node._kids.interval_json()
                           for kid in leftmost_kids})
 
+  async def _receive_from_greater_height_source(self, subscriber):
+    self._controller.send(subscriber, self._subscription_started([self._controller.new_handle(subscriber['id'])]))
+    edges, sender_id = await self._controller.listen(type='subscription_edges')
+    proxies = edges[self._node.id]
+    if len(proxies) != 1:
+      raise errors.InternalError(
+          "A receiving data node subscribed to a link of greater height, but did not get a unique proxy to pair with.")
+    start_subscription, _sender_id = await self._controller.listen(type='start_subscription') # It will come from proxy
+    return start_subscription
+
   async def run(self, controller: 'TransactionRoleController'):
     self._controller = controller
 
-    controller.send(
+    self._controller.send(
         self._requester,
         messages.io.hello_parent(
-            kid=controller.new_handle(self._requester['id']), kid_summary=controller.node._kid_summary_message()))
+            kid=self._controller.new_handle(self._requester['id']), kid_summary=self._node._kid_summary_message()))
 
-    start_subscription, _sender_id = await controller.listen(type='start_subscription')
+    start_subscription, _sender_id = await self._controller.listen(type='start_subscription')
+    while start_subscription['height'] > self._node._height:
+      start_subscription = await self._receive_from_greater_height_source(start_subscription['subscriber'])
+
     subscriber = start_subscription['subscriber']
-    while start_subscription['height'] > controller.node._height:
-      # The subscriber has too great a height, tell it to have its kids subscribe to this node again.
-      controller.send(subscriber, self._subscription_started([controller.new_handle(subscriber['id'])]))
-      edges, sender_id = await controller.listen(type='subscription_edges')
-      proxies = edges[controller.node.id]
-      if len(proxies) != 1:
-        raise errors.InternalError(
-            "A receiving data node subscribed to a link of greater height, but did not get a unique proxy to pair with."
-        )
-      proxy = proxies[0]
-      start_subscription, _sender_id = await controller.listen(type='start_subscription') # It will come from proxy
-      subscriber = start_subscription['subscriber']
+    leftmost_kids = await self._enlist_kids_and_await_hellos()
+    self._controller.send(
+        subscriber,
+        self._subscription_started(
+            leftmost_kids=[self._controller.transfer_handle(kid, subscriber['id']) for kid in leftmost_kids]))
 
-    kid_ids = set(controller.node._kids)
+    # We don't actually need to do anything with these edges and only listen for this
+    # message so that it isn't lost.
+    await self._controller.listen(type='subscription_edges')
+
+  async def _enlist_kids_and_await_hellos(self):
+    kid_ids = set(self._node._kids)
     for kid_id in kid_ids:
-      kid = controller.node._kids[kid_id]
-      controller.enlist(kid, ReceiveStartSubscription, {})
+      kid = self._node._kids[kid_id]
+      self._controller.enlist(kid, ReceiveStartSubscription, dict(requester=self._controller.new_handle(kid_id)))
 
     leftmost_kids = []
     while kid_ids:
-      hello_parent, kid_id = await controller.listen(type='hello_parent')
+      hello_parent, kid_id = await self._controller.listen(type='hello_parent')
       kid_ids.remove(kid_id)
-      leftmost_kids.append(controller.transfer_handle(hello_parent['kid'], subscriber['id']))
+      leftmost_kids.append(hello_parent['kid'])
 
-    controller.send(subscriber, self._subscription_started(leftmost_kids))
-
-    await controller.listen(type='subscription_edges')
-    # We don't actually need to do anything with these edges and only listen for this
-    # message that it isn't lost.
+    return leftmost_kids
