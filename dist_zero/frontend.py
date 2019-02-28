8728c3914dfa923d8a3bcd7d5a0ad7d87e750db3
Korei Klein <korei.klein@gmail.com>
2019-02-28 15:33:41 -0500

    Scaffold a DistZero frontend object.

---
+++ dist_zero/frontend.py
@@ -0,0 +1,8 @@
+class DistZero(object):
+  '''
+  Each instance of the `DistZero` class serves as a frontend to all of DistZero.
+
+  It can be used to create `Expression` objects, and compile them into distributed programs to be
+  run by the DistZero runtime.
+  '''
+  pass
