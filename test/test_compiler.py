1d6354d0d06e590affff44792f010f9cd49a19b6
Korei Klein <korei.klein@gmail.com>
2019-03-05 19:10:44 -0500

    Factor cardinality tests into a class.

--- test/test_compiler.py
+++ test/test_compiler.py
@@ -5,89 +5,87 @@ from dist_zero import types, primitive
 from dist_zero.compiler import cardinality, normalize
 
 
-def _norm_project_counter(normExpr):
-  return normalize.Applied(arg=normExpr, p=primitive.Project('counter'))
+class TestCardinality(object):
+  def _norm_project_counter(self, normExpr):
+    return normalize.Applied(arg=normExpr, p=primitive.Project('counter'))
 
+  def test_cardinalizer_simple(self):
+    c = cardinality.Cardinalizer()
 
-def test_cardinalizer_simple():
-  c = cardinality.Cardinalizer()
+    norm_web_input = normalize.NormWebInput(domain_name='www.example.com')
+    zero = normalize.NormConstant(0)
+    one = normalize.NormConstant(1)
+    counter = self._norm_project_counter(norm_web_input.element_of)
+    record = normalize.NormRecord([
+        ('a', one),
+        ('b', counter.case_of('nonzero')),
+    ])
+    expr = normalize.NormCase(
+        base=counter, items=[
+            ('zero', zero),
+            ('nonzero', record),
+        ])
+    assert 0 == len(c.cardinalize(norm_web_input))
+    assert 0 == len(c.cardinalize(zero))
+    assert 0 == len(c.cardinalize(one))
 
-  norm_web_input = normalize.NormWebInput(domain_name='www.example.com')
-  zero = normalize.NormConstant(0)
-  one = normalize.NormConstant(1)
-  counter = _norm_project_counter(norm_web_input.element_of)
-  record = normalize.NormRecord([
-      ('a', one),
-      ('b', counter.case_of('nonzero')),
-  ])
-  expr = normalize.NormCase(
-      base=counter, items=[
-          ('zero', zero),
-          ('nonzero', record),
-      ])
-  assert 0 == len(c.cardinalize(norm_web_input))
-  assert 0 == len(c.cardinalize(zero))
-  assert 0 == len(c.cardinalize(one))
+    elt_cardinality = cardinality.Cardinality(list_exprs=[norm_web_input])
+    assert elt_cardinality.equal(c.cardinalize(norm_web_input.element_of))
+    assert elt_cardinality.equal(c.cardinalize(expr))
+    assert elt_cardinality.equal(c.cardinalize(record))
 
-  elt_cardinality = cardinality.Cardinality(list_exprs=[norm_web_input])
-  assert elt_cardinality.equal(c.cardinalize(norm_web_input.element_of))
-  assert elt_cardinality.equal(c.cardinalize(expr))
-  assert elt_cardinality.equal(c.cardinalize(record))
+  def test_cardinalizer_nested(self):
+    norm_web_input = normalize.NormWebInput(domain_name='www.example.com')
+    typical_web_node_list = self._norm_project_counter(norm_web_input.element_of)
 
+    each_sum = normalize.Applied(
+        arg=normalize.NormRecord([('left', typical_web_node_list.element_of), ('right', normalize.NormConstant(0))]),
+        p=primitive.Plus(types.Int32))
 
-def test_cardinalizer_nested():
-  norm_web_input = normalize.NormWebInput(domain_name='www.example.com')
-  typical_web_node_list = _norm_project_counter(norm_web_input.element_of)
+    mapped = normalize.NormListOp(base=typical_web_node_list, opVariant='map', element_expr=each_sum)
 
-  each_sum = normalize.Applied(
-      arg=normalize.NormRecord([('left', typical_web_node_list.element_of), ('right', normalize.NormConstant(0))]),
-      p=primitive.Plus(types.Int32))
+    c = cardinality.Cardinalizer()
+    assert len(c.cardinalize(each_sum)) == 2
+    assert cardinality.Cardinality(list_exprs=[norm_web_input, typical_web_node_list]).equal(c.cardinalize(each_sum))
+    assert cardinality.Cardinality(list_exprs=[norm_web_input]).equal(c.cardinalize(mapped))
 
-  mapped = normalize.NormListOp(base=typical_web_node_list, opVariant='map', element_expr=each_sum)
+  def test_cardinality_trie(self):
+    a, b, c, d, e, f, g = [normalize.NormWebInput(f"www{i}.example.com") for i in range(7)]
+    card = lambda elts: cardinality.Cardinality(elts)
+    trie = cardinality.CardinalityTrie.build_trie([
+        card([]),
+        card([a, b, c]),
+        card([a, b, d, e, f]),
+        card([a, b, d, e, f, g]),
+        card([a, b]),
+    ])
+    included_cards = list(trie.cardinalities())
+    assert len(included_cards) == 5
 
-  c = cardinality.Cardinalizer()
-  assert len(c.cardinalize(each_sum)) == 2
-  assert cardinality.Cardinality(list_exprs=[norm_web_input, typical_web_node_list]).equal(c.cardinalize(each_sum))
-  assert cardinality.Cardinality(list_exprs=[norm_web_input]).equal(c.cardinalize(mapped))
+    assert card([]).equal(trie.cardinality)
+    kids = dict(trie.items())
+    assert len(kids) == 1
+    keys, second_trie = kids.pop(a)
+    assert [a, b] == keys
 
+    second_kids = dict(second_trie.items())
+    assert 2 == len(second_kids)
+    assert card([a, b]).equal(second_trie.cardinality)
+    keys, left_trie = second_kids.pop(c)
+    assert [c] == keys
+    assert card([a, b, c]).equal(left_trie.cardinality)
+    assert 0 == len(list(left_trie.items()))
 
-def test_cardinality_trie():
-  a, b, c, d, e, f, g = [normalize.NormWebInput(f"www{i}.example.com") for i in range(7)]
-  card = lambda elts: cardinality.Cardinality(elts)
-  trie = cardinality.CardinalityTrie.build_trie([
-      card([]),
-      card([a, b, c]),
-      card([a, b, d, e, f]),
-      card([a, b, d, e, f, g]),
-      card([a, b]),
-  ])
-  included_cards = list(trie.cardinalities())
-  assert len(included_cards) == 5
+    keys, right_trie = second_kids.pop(d)
+    assert [d, e, f] == keys
+    right_kids = dict(right_trie.items())
+    assert card([a, b, d, e, f]).equal(right_trie.cardinality)
+    assert 1 == len(right_kids)
 
-  assert card([]).equal(trie.cardinality)
-  kids = dict(trie.items())
-  assert len(kids) == 1
-  keys, second_trie = kids.pop(a)
-  assert [a, b] == keys
-
-  second_kids = dict(second_trie.items())
-  assert 2 == len(second_kids)
-  assert card([a, b]).equal(second_trie.cardinality)
-  keys, left_trie = second_kids.pop(c)
-  assert [c] == keys
-  assert card([a, b, c]).equal(left_trie.cardinality)
-  assert 0 == len(list(left_trie.items()))
-
-  keys, right_trie = second_kids.pop(d)
-  assert [d, e, f] == keys
-  right_kids = dict(right_trie.items())
-  assert card([a, b, d, e, f]).equal(right_trie.cardinality)
-  assert 1 == len(right_kids)
-
-  keys, final_trie = right_kids.pop(g)
-  assert [g] == keys
-  assert 0 == len(list(final_trie.items()))
-  assert card([a, b, d, e, f, g]).equal(final_trie.cardinality)
+    keys, final_trie = right_kids.pop(g)
+    assert [g] == keys
+    assert 0 == len(list(final_trie.items()))
+    assert card([a, b, d, e, f, g]).equal(final_trie.cardinality)
 
 
 @pytest.mark.asyncio
